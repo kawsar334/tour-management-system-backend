b363d9570e23b327d00c83c98c925c539f99ca6b
kawsar firoz <kawsarfiroz11@gmail.com>
2025-11-06 23:48:24 +0800

    temp save

--- src/modules/user/user.route.ts
+++
@@ -1,9 +0,0 @@
-import { UserController } from "./user.controller.js";
-
-import express from "express";
-
-const router = express.Router();
-router.post("/register", UserController.createUser);
-
-
-export const userRoutes=router;
